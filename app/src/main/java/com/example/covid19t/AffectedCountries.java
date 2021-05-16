82ba4558b23998fc41d5e47f6df312c302652ae8
cekic.melihsah <cekic.melihsah@std.izu.edu.tr>
2021-05-16 18:37:25 +0300

    guncelleme

---
+++ app/src/main/java/com/example/covid19t/AffectedCountries.java
@@ -0,0 +1,140 @@
+package com.example.covid19t;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AffectedCountries extends AppCompatActivity {
+
+    EditText edtSearch;
+    ListView listView;
+    SimpleArcLoader simpleArcLoader;
+
+    public static List<CountryModel> countryModelsList = new ArrayList<>();
+    CountryModel countryModel;
+    MyCustomAdapter myCustomAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_affected_countries);
+
+        edtSearch = findViewById(R.id.edtSearch);
+        listView = findViewById(R.id.listView);
+        simpleArcLoader = findViewById(R.id.loader);
+
+        getSupportActionBar().setTitle("Affected Countries");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        fetchData();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                startActivity(new Intent(getApplicationContext(),DetailActivity.class).putExtra("position",position));
+            }
+        });
+
+
+        edtSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                myCustomAdapter.getFilter().filter(s);
+                myCustomAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==android.R.id.home)
+            finish();
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void fetchData() {
+
+        String url  = "https://corona.lmao.ninja/v2/countries/";
+
+        simpleArcLoader.start();
+
+        StringRequest request = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONArray jsonArray = new JSONArray(response);
+
+                            for(int i=0;i<jsonArray.length();i++){
+
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                                String countryName = jsonObject.getString("country");
+                                String cases = jsonObject.getString("cases");
+                                String todayCases = jsonObject.getString("todayCases");
+                                String deaths = jsonObject.getString("deaths");
+                                String todayDeaths = jsonObject.getString("todayDeaths");
+                                String recovered = jsonObject.getString("recovered");
+                                String active = jsonObject.getString("active");
+                                String critical = jsonObject.getString("critical");
+
+                                JSONObject object = jsonObject.getJSONObject("countryInfo");
+                                String flagUrl = object.getString("flag");
+
+                                countryModel = new CountryModel(flagUrl,countryName,cases,todayCases,deaths,todayDeaths,recovered,active,critical);
+                                countryModelsList.add(countryModel);
+
+
+                            }
+
+                            myCustomAdapter = new MyCustomAdapter(AffectedCountries.this,countryModelsList);
+                            listView.setAdapter(myCustomAdapter);
+                            simpleArcLoader.stop();
+                            simpleArcLoader.setVisibility(View.GONE);
+
+
+
+
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            simpleArcLoader.stop();
+                            simpleArcLoader.setVisibility(View.GONE);
+                        }
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                simpleArcLoader.stop();
+                simpleArcLoader.setVisibility(View.GONE);
+                Toast.makeText(AffectedCountries.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
+
+
+    }
+
+}
